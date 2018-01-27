c5982f4d7c5d81ef4d8ea550e902868b94242881
TimothyKBook <TimothyKBook@gmail.com>
2018-01-27 13:48:08 -0500

    Fixed email body

--- emailer.py
+++ emailer.py
@@ -13,7 +13,7 @@ class Emailer:
 
     def buyEmail(self, w, p):
         subj_line = "Subject: Ethel just bought ETH\n"
-        line1 = "ETH price at purchase: $%5.2f" % p.current_price
+        line1 = "ETH price at purchase: $%5.2f\n" % p.current_price
         line2 = "Wallet now contains %1.6f ETH (worth $%5.2f)" \
                 % (w.n_eth, w.n_eth * p.current_price)
         body = subj_line + line1 + line2
@@ -21,7 +21,7 @@ class Emailer:
 
     def sellEmail(self, w, p):
         subj_line = "Subject: Ethel just sold ETH\n"
-        line1 = "ETH price at sell: $%5.2f" % p.current_price
+        line1 = "ETH price at sell: $%5.2f\n" % p.current_price
         line2 = "Wallet now contains $%5.2f (worth %1.6f ETH)" \
                 % (w.n_usd, w.n_usd / p.current_price)
         body = subj_line + line1 + line2
